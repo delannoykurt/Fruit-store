2bb89d5c1b23dba1604e981e707448a447fcf2a9
delannoykurt <delannoykurt666@gmail.com>
2025-05-06 19:37:18 +0200

    add controller for routing add

--- main.go
+++ main.go
@@ -4,13 +4,24 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 )
+
+// Stock temporaire en mémoire
+var stock []Product
+
+type Product struct {
+	Fruit  int
+	From   string
+	Weight float32
+	Date   time.Time
+}
 
 func main() {
 	http.HandleFunc("/", homeHandler)
-
-	// Fichiers statiques si besoin (CSS, images...)
-	// http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.HandleFunc("/add", addHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	println("Serveur démarré sur http://localhost:8080")
 	err := http.ListenAndServe(":8080", nil)
@@ -26,4 +37,30 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	tmpl.Execute(w, nil)
+}
+
+func addHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		tmpl := template.Must(template.ParseFiles("templates/add.html"))
+		tmpl.Execute(w, nil)
+		return
+	}
+
+	// Traitement du formulaire (POST)
+	r.ParseForm()
+
+	fruit, _ := strconv.Atoi(r.FormValue("fruit"))
+	from := r.FormValue("from")
+	weight, _ := strconv.ParseFloat(r.FormValue("weight"), 32)
+
+	// Ajout au stock en mémoire
+	p := Product{
+		Fruit:  fruit,
+		From:   from,
+		Weight: float32(weight),
+		Date:   time.Now(),
+	}
+	stock = append(stock, p)
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
